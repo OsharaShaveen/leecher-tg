4460d8ed1834b3432d994039a8b028e97ee8e473
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2022-02-09 17:56:25 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -88,7 +88,7 @@ class Progress:
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
+            #tmp += "\n│"+"\n╰── ⌊ 𝙒𝙃𝙄𝙏𝙀 𝘿𝙀𝙑𝙄𝙇 ⌉"
             )
             try:
                 if not self._mess.photo:
